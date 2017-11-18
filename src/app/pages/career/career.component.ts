30b0e7b17e02b6521dc0f5042202159be18ac09f
mimiking <e.study@outlook.com>
2017-11-19 03:33:24 +0900

    change names

--- src/app/pages/career/career.component.ts
+++ src/app/pages/career/career.component.ts
@@ -8,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class CareerComponent implements OnInit {
 
   careers: any = [
-    { code: '01', name: '业余', name_jp: '', name_en: '', image: './assets/career/Amateur.png' },
-    { code: '02', name: '纳斯卡', name_jp: '', name_en: '' },
-    { code: '03', name: '职业/业余', name_jp: '', name_en: '' },
-    { code: '04', name: '职业', name_jp: '', name_en: '' },
-    { code: '05', name: '专家', name_jp: '', name_en: '' },
-    { code: '06', name: '大师', name_jp: '', name_en: '' },
-    { code: '07', name: '精英', name_jp: '', name_en: '' },
-    { code: '08', name: '传奇', name_jp: '', name_en: '' },
-    { code: '09', name: '专属', name_jp: '', name_en: '' },
+    { code: '01', name: '业余(AMATEUR)', name_jp: '', name_en: '', image: './assets/career/Amateur.png' },
+    { code: '02', name: '纳斯卡(NASCAR)', name_jp: '', name_en: '' },
+    { code: '03', name: '职业(PRO)/业余(AM)', name_jp: '', name_en: '' },
+    { code: '04', name: '职业(PRO)', name_jp: '', name_en: '' },
+    { code: '05', name: '专家(EXPERT)', name_jp: '', name_en: '' },
+    { code: '06', name: '大师(MASTER)', name_jp: '', name_en: '' },
+    { code: '07', name: '精英(ELITE)', name_jp: '', name_en: '' },
+    { code: '08', name: '传奇(LEGEND)', name_jp: '', name_en: '' },
+    { code: '09', name: '专属(EXCLUSIVE)', name_jp: '', name_en: '' },
     { code: '10', name: '...', name_jp: '', name_en: '' }
   ];
 
